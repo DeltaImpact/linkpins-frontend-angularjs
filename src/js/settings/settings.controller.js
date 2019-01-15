5e7b986f65b209d3426839d501ac2cde2cbe49b2
DeltaImpact <p.grachev.vik@gmail.com>
2019-01-15 13:08:53 +0300

    feat Changed the settings page to look like linkpins-frontend settings page.

--- src/js/settings/settings.controller.js
+++ src/js/settings/settings.controller.js
@@ -1,34 +1,49 @@
 class SettingsCtrl {
-  constructor(User, $state) {
-    'ngInject';
+  constructor(User, $state, $scope) {
+    "ngInject";
 
     this._User = User;
     this._$state = $state;
+    this._$scope = $scope;
+    // debugger;
+    // $scope.$apply() 
+    this.userInfoLoaded = false;
+    this.mode = "profile";
 
-    this.formData = {
-      email: User.current.email,
-      bio: User.current.bio,
-      image: User.current.image,
-      username: User.current.username
-    }
+    User.dataAboutUser(User.current.username).then(dataAboutUser => {
+      this.userInfoLoaded = true;
+      this.userData = dataAboutUser;
+      this.formData = {
+        email: this.userData.email,
+        firstName: this.userData.firstName,
+        surName: this.userData.surname,
+        gender: this.userData.gender,
+        username: this.userData.userName
+      };
+      $scope.$apply() 
+      // debugger;
+    });
+
+    // debugger;
+    // this.dataAboutUser = User.dataAboutUser.bind(User.current.username);
+
+    // dataAboutUser
 
     this.logout = User.logout.bind(User);
-
   }
 
   submitForm() {
     this.isSubmitting = true;
     this._User.update(this.formData).then(
-      (user) => {
-        this._$state.go('app.profile.main', {username:user.username})
+      user => {
+        this._$state.go("app.profile.main", { username: user.username });
       },
-      (err) => {
+      err => {
         this.isSubmitting = false;
         this.errors = err.data.errors;
       }
-    )
+    );
   }
-
 }
 
 export default SettingsCtrl;
