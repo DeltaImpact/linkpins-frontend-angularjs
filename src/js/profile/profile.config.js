4cbc4a4b39f58b0652e8e0cc47e6889a0f775f08
DeltaImpact <p.grachev.vik@gmail.com>
2019-01-17 15:38:09 +0300

    feat Added loading profile from back-end to profile page.

--- src/js/profile/profile.config.js
+++ src/js/profile/profile.config.js
@@ -9,21 +9,14 @@ function ParsingConfig($stateProvider) {
     controllerAs: '$ctrl',
     templateUrl: 'profile/profile.html',
     title: 'Profile',
-    // resolve: {
-    //   auth: function(User) {
-    //     return User.ensureAuthIs(true);
-    //   }
-    // }
-    // resolve: {
-    //   profile: function(Profile, $state, $stateParams) {
-    //     debugger
-    //     return Profile.get($stateParams.username).then(
-    //       (profile) => profile,
-    //       (err) => $state.go('app.home')
-    //     )
-    //   }
-    // }
-  });
+  })
+  .state('app.profile.main', {
+    url:'',
+    controller: 'ProfileArticlesCtrl',
+    controllerAs: '$ctrl',
+    // templateUrl: 'profile/profile-articles.html',
+    title: 'Profile'
+  })
 };
 
 export default ParsingConfig;
--- src/js/profile/profile.controller.js
+++ src/js/profile/profile.controller.js
@@ -1,52 +1,35 @@
 class SettingsCtrl {
-  constructor(User, Parse, $state, $scope) {
+  constructor(User, Parse, $state, $scope, $stateParams) {
     "ngInject";
 
     this._User = User;
     this._$state = $state;
     this._$scope = $scope;
     this._Parse = Parse;
+    this._$stateParams = $stateParams;
 
-    this.isLoading = false;
-    this.ToParseUrl = "";
-    this.ToParseUrl = "https://habr.com/ru/post/183008/";
-    this.parsing = Parse.parsing.bind(this.ToParseUrl);
-  }
-
-  parseSite() {
+    // this.isLoading = false;
     this.isLoading = true;
-    this.errors = null;
-    this._Parse.parsing(this.ToParseUrl).then(
-      item => {
-        this.item = item;
-        this.pinPreview = {};
-        this.pinPreview.header = item.header;
-        this.pinPreview.images = item.images[0];
-        this.pinPreview.possibleDescriptions = item.possibleDescriptions[0];
-        this.pinPreview.url = item.url; 
+
+    User.dataAboutUser($stateParams.username).then(
+      dataAboutUser => {
         this.isLoading = false;
-        this._$scope.$apply();
+        this.userData = dataAboutUser;
+        // debugger
+        $scope.$apply();
       },
       errors => {
+        this.isLoading = false;
+        this.errors = errors.messages;
         // debugger
 
-        this.errors = errors.messages;
-        // this.errors = errors;
-        this.isLoading = false;
-        this._$scope.$apply();
+
+        $scope.$apply();
+        // debugger;
       }
     );
   }
 
-  setPinPreviewImage(url) {
-    console.log(url);
-    this.pinPreview.images = url;
-  }
-
-  setPinPreviewDescription(text) {
-    console.log(text);
-    this.pinPreview.possibleDescriptions = text;
-  }
 }
 
 export default SettingsCtrl;
